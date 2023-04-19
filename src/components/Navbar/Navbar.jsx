437664c84381933a7dfa17a15417f3382affd79e
Rahlien <rahlien@DESKTOP-1KBBDBH.localdomain>
2023-04-18 23:34:17 -0400

    changed header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
               <motion.div whileInView={{ x: [300, 0] }} transition={{ duration: 0.85, ease: 'easeOut' }}>
                 <HiX onClick={() => setToggle(false)}/>
                 <ul>
-                {['home', 'about', 'work', 'skills', 'contact' ].map((item) => (
+                {['home', 'about', 'work', 'skills', 'testimonials', 'contact' ].map((item) => (
                   <li className='app__flex p-text' key={item}>
                     <a href={`#${item}`} onClick={() => setToggle(false)}> {item} </a>
                   </li>
